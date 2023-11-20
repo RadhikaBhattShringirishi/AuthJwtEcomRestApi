ff70f1d37736d8bf14bd501532d73a9965cf6340
radhi <radhikahatt.ec@gmail.com>
2023-11-20 17:07:08 +0300

    Created  Main project

---
+++ src/main/java/com/ecom/ete/JwtEcomApiApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.ete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtEcomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtEcomApiApplication.class, args);
+		System.out.println("Hii");
+	}
+
+}
